03d95c5adfb850b5d90cf973100efcf3556b11d2
tylernpc <tylerlegm@gmail.com>
2026-05-11 22:56:04 -0500

    dev: splitter piece implemented along with file extension mapping

--- repo-sage-web-app/scripts/ingest.js
+++ repo-sage-web-app/scripts/ingest.js
@@ -1,6 +1,15 @@
+import path from "node:path";
 import { GithubRepoLoader } from "@langchain/community/document_loaders/web/github";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
-const run = async () => {
+const langMap = {
+  ".js": "js",
+  ".ts": "ts",
+  ".py": "python",
+  ".md": "markdown",
+};
+
+const load = async () => {
   const loader = new GithubRepoLoader(
     //todo: tyler - remove link and switch to a user input based link
     "https://github.com/langchain-ai/langchainjs",
@@ -13,8 +22,33 @@ const run = async () => {
   );
 
   const docs = await loader.load();
+  const chunks = await split(docs);
+};
 
-  console.log({ docs });
+const split = async (docs) => {
+  const splitDocs = await Promise.all(
+    docs.map(async (doc) => {
+      const ext = path.extname(doc.metadata.source ?? "");
+      const lang = langMap[ext] ?? "";
+
+      const splitter = RecursiveCharacterTextSplitter.fromLanguage(lang, {
+        chunkSize: 1000,
+        chunkOverlap: 200,
+      });
+
+      return splitter.splitDocuments([doc]);
+    }),
+  );
+
+  return splitDocs.flat();
+};
+
+const run = async () => {
+  console.log("Starting...");
+
+  await load();
+
+  console.log("Repo Ingested...");
 };
 
 run();
