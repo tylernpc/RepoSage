1e1e61c01db05b8d82012a310371d007a410b1c3
tylernpc <tylerlegm@gmail.com>
2026-05-12 22:08:04 -0500

    dev: general changes

--- repo-sage-web-app/lib/embeddings.ts
+++ repo-sage-web-app/lib/embeddings.ts
@@ -1,5 +1,5 @@
 import { OpenAIEmbeddings } from "@langchain/openai";
 
 export const embeddings = new OpenAIEmbeddings({
-  model: "text-embedding-3-large",
+  model: "text-embedding-3-small",
 });
